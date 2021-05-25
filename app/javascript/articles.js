c2e7f7dd19141e8ea89c6f123b050c966e48192d
arata-t <taniguro.arata@gmail.com>
2021-05-26 00:09:15 +0900

    pdf以外は起動しない

--- app/javascript/articles.js
+++ app/javascript/articles.js
@@ -3,24 +3,26 @@ $(function(){
 
     // ファイル情報を取得
     var fileData = e.target.files[0];
-  
-    //サムネイル表示エリアのエレメント取得
-    var object = $(this).parent().children('object').get(0);
+    if (fileData.type == "application/pdf"){
+      console.log(fileData.type)
+      //サムネイル表示エリアのエレメント取得
+      var object = $(this).parent().children('object').get(0);
+
+      // FileReaderオブジェクトを使ってファイル読み込み
+      var reader = new FileReader();
+      // ファイル読取り成功時処理
+      reader.onload = function() {
     
-    // FileReaderオブジェクトを使ってファイル読み込み
-    var reader = new FileReader();
-    // ファイル読取り成功時処理
-    reader.onload = function() {
-  
-        //ノードの複製
-        var cln = object.cloneNode(true);
-  
-        //複製したノードのdata要素をにFileAPIの読み込み結果をセット
-        cln.setAttribute("data",reader.result);
-  
-        object.parentNode.replaceChild(cln, object);
+          //ノードの複製
+          var cln = object.cloneNode(true);
+    
+          //複製したノードのdata要素をにFileAPIの読み込み結果をセット
+          cln.setAttribute("data",reader.result);
+    
+          object.parentNode.replaceChild(cln, object);
+      }
+      // ファイル読み込みを実行
+      reader.readAsDataURL(fileData);
     }
-    // ファイル読み込みを実行
-    reader.readAsDataURL(fileData);
   });
 });
